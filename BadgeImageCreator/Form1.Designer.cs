ac2d16b0fa4362a8dbab775791bf16260d4f57e6
David-John Miller <anoyomouse@petermiller.co.za>
2014-06-10 19:35:02 +0200

    Inital commit

---
+++ BadgeImageCreator/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace BadgeImageCreator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.cmdProcess = new System.Windows.Forms.Button();
+			this.pbInt = new AForge.Controls.PictureBox();
+			this.pbDest = new AForge.Controls.PictureBox();
+			this.pbSource = new AForge.Controls.PictureBox();
+			this.pcFullImage = new AForge.Controls.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pbInt)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbDest)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pcFullImage)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// cmdProcess
+			// 
+			this.cmdProcess.Location = new System.Drawing.Point(320, 194);
+			this.cmdProcess.Name = "cmdProcess";
+			this.cmdProcess.Size = new System.Drawing.Size(104, 23);
+			this.cmdProcess.TabIndex = 2;
+			this.cmdProcess.Text = "Process";
+			this.cmdProcess.UseVisualStyleBackColor = true;
+			this.cmdProcess.Click += new System.EventHandler(this.cmdProcess_Click);
+			// 
+			// pbInt
+			// 
+			this.pbInt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbInt.Image = null;
+			this.pbInt.Location = new System.Drawing.Point(281, 12);
+			this.pbInt.Name = "pbInt";
+			this.pbInt.Size = new System.Drawing.Size(265, 177);
+			this.pbInt.TabIndex = 3;
+			this.pbInt.TabStop = false;
+			// 
+			// pbDest
+			// 
+			this.pbDest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbDest.Image = null;
+			this.pbDest.Location = new System.Drawing.Point(552, 12);
+			this.pbDest.Name = "pbDest";
+			this.pbDest.Size = new System.Drawing.Size(265, 177);
+			this.pbDest.TabIndex = 3;
+			this.pbDest.TabStop = false;
+			// 
+			// pbSource
+			// 
+			this.pbSource.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbSource.Image = ((System.Drawing.Image)(resources.GetObject("pbSource.Image")));
+			this.pbSource.Location = new System.Drawing.Point(11, 12);
+			this.pbSource.Name = "pbSource";
+			this.pbSource.Size = new System.Drawing.Size(265, 177);
+			this.pbSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pbSource.TabIndex = 3;
+			this.pbSource.TabStop = false;
+			// 
+			// pcFullImage
+			// 
+			this.pcFullImage.Cursor = System.Windows.Forms.Cursors.Default;
+			this.pcFullImage.Image = global::BadgeImageCreator.Properties.Resources.Mouse_Flower;
+			this.pcFullImage.Location = new System.Drawing.Point(0, 0);
+			this.pcFullImage.Name = "pcFullImage";
+			this.pcFullImage.Size = new System.Drawing.Size(673, 673);
+			this.pcFullImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pcFullImage.TabIndex = 3;
+			this.pcFullImage.TabStop = false;
+			this.pcFullImage.Click += new System.EventHandler(this.pcFullImage_Click);
+			this.pcFullImage.Paint += new System.Windows.Forms.PaintEventHandler(this.pcFullImage_Paint);
+			this.pcFullImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.pcFullImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			this.pcFullImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// panel1
+			// 
+			this.panel1.AutoScroll = true;
+			this.panel1.BackColor = System.Drawing.Color.Transparent;
+			this.panel1.Controls.Add(this.pcFullImage);
+			this.panel1.Location = new System.Drawing.Point(12, 233);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(804, 489);
+			this.panel1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(833, 727);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pbInt);
+			this.Controls.Add(this.pbDest);
+			this.Controls.Add(this.pbSource);
+			this.Controls.Add(this.cmdProcess);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pbInt)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbDest)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pcFullImage)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button cmdProcess;
+		private AForge.Controls.PictureBox pbSource;
+		private AForge.Controls.PictureBox pbDest;
+		private AForge.Controls.PictureBox pbInt;
+		private AForge.Controls.PictureBox pcFullImage;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ BadgeImageCreator/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+
+namespace BadgeImageCreator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void cmdProcess_Click(object sender, EventArgs e)
+		{
+			Bitmap b = new Bitmap(pbSource.Width, pbSource.Height);
+			Graphics g = Graphics.FromImage(b);
+			g.DrawImage(pbSource.Image, new Rectangle(0,0,pbSource.Width, pbSource.Height));
+			g.Dispose();
+
+			var greyb = AForge.Imaging.Filters.Grayscale.CommonAlgorithms.BT709.Apply(b);
+			pbInt.Image = greyb;
+
+			//BaseInPlacePartialFilter filter = new AForge.Imaging.Filters.FloydSteinbergDithering();
+			BaseInPlacePartialFilter filter = new AForge.Imaging.Filters.SierraDithering();
+
+			var ditheredb = filter.Apply(greyb);
+			pbDest.Image = ditheredb;
+		}
+
+		private void DitheringModes()
+		{
+			List<BaseInPlacePartialFilter> filters = new List<BaseInPlacePartialFilter>();
+			
+			filters.Add(new BurkesDithering());
+			filters.Add(new FloydSteinbergDithering());
+			filters.Add(new JarvisJudiceNinkeDithering());
+			filters.Add(new SierraDithering());
+			filters.Add(new StuckiDithering());
+		}
+
+		private void rectangleShape1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pcFullImage_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private int BoxWidth = 4;
+
+		private bool _placed = false;
+		private Rectangle _selection;
+
+		private Point _movement_point;
+
+		private BoxState _state;
+		private BoxFlags _box_flags;
+
+		private BoxFlags GetFlags(int x, int y)
+		{
+			BoxFlags resizeMask = BoxFlags.None;
+
+			if ((x > _selection.X - BoxWidth && x < _selection.X + _selection.Width + BoxWidth) &&
+				(y > _selection.Y - BoxWidth && y < _selection.Y + _selection.Height + BoxWidth))
+			{
+				// We're resizing, or moving!
+			}
+			else
+			{
+				return BoxFlags.None;
+			}
+
+			if ((x > _selection.X - BoxWidth && x < _selection.X + _selection.Width + BoxWidth) &&
+				(y > _selection.Y - BoxWidth && y < _selection.Y + BoxWidth))
+			{
+				resizeMask |= BoxFlags.Top;
+			}
+
+			if ((x > _selection.X - BoxWidth && x < _selection.X + BoxWidth) &&
+				(y > _selection.Y - BoxWidth && y < _selection.Y + _selection.Height + BoxWidth))
+			{
+				resizeMask |= BoxFlags.Left;
+			}
+
+			if ((x > _selection.X - BoxWidth && x < _selection.X + _selection.Width + BoxWidth) &&
+				(y > _selection.Y + _selection.Height - BoxWidth && y < _selection.Y + _selection.Height + BoxWidth))
+			{
+				resizeMask |= BoxFlags.Bottom;
+			}
+
+			if ((x > _selection.X + _selection.Width - BoxWidth && x < _selection.X + _selection.Width + BoxWidth) &&
+				(y > _selection.Y - BoxWidth && y < _selection.Y + _selection.Height + BoxWidth))
+			{
+				resizeMask |= BoxFlags.Right;
+			}
+
+			if (resizeMask == BoxFlags.None)
+				resizeMask = BoxFlags.All;
+
+			return resizeMask;
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			// Starting point of the selection:
+			if (e.Button == MouseButtons.Left)
+			{
+				if (!_placed)
+				{
+					_selection = new Rectangle(new Point(e.X, e.Y), new Size());
+					_placed = true;
+
+					_state = BoxState.Placing;
+				}
+				else
+				{
+					if ((e.X > _selection.X - BoxWidth && e.X < _selection.X + _selection.Width + BoxWidth) &&
+						(e.Y > _selection.Y - BoxWidth && e.Y < _selection.Y + _selection.Height + BoxWidth))
+					{
+						var resizeMask = GetFlags(e.X, e.Y);
+						if (resizeMask == BoxFlags.All)
+						{
+							_state = BoxState.Moving;
+							_box_flags = resizeMask;
+							_movement_point = new Point(e.X - _selection.X, e.Y - _selection.Y);
+
+							this.Text = string.Format("MouseDown({0}, {1}, {2})", e.X, e.Y, _state);
+						}
+						else if (resizeMask != BoxFlags.None)
+						{
+							_state = BoxState.Resizing;
+							_box_flags = resizeMask;
+		
+							this.Text = string.Format("MouseDown({0}, {1}, {2})", e.X, e.Y, _state);
+						}
+					}
+					else
+					{
+						_selection = new Rectangle(new Point(e.X, e.Y), new Size());
+						_state = BoxState.Placing;
+					}
+				}
+			}
+
+			pcFullImage.Refresh();
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			// Update the actual size of the selection:
+			if (_state == BoxState.Placing)
+			{
+				float ratiow = 176f / 264f;
+
+				_selection.X = Math.Min(e.X, _selection.X);
+
+				_selection.Width = Math.Abs(e.X - _selection.X);
+				_selection.Height = (int)(_selection.Width * ratiow);
+
+				// Redraw the picturebox:
+				pcFullImage.Refresh();
+			}
+			else if (_state == BoxState.Moving)
+			{
+				// Get Delta X of movement
+				_selection.X = e.X - _movement_point.X;
+				_selection.Y = e.Y - _movement_point.Y;
+
+				pcFullImage.Refresh();
+			}
+
+			if (_state == BoxState.None)
+			{
+				if ((e.X > _selection.X - BoxWidth && e.X < _selection.X + _selection.Width + BoxWidth) &&
+					(e.Y > _selection.Y - BoxWidth && e.Y < _selection.Y + _selection.Height + BoxWidth))
+				{
+					var flags = GetFlags(e.X, e.Y);
+					if (flags == BoxFlags.All)
+						pcFullImage.Cursor = Cursors.SizeAll;
+					else if (flags == BoxFlags.Left || flags == BoxFlags.Right)
+						pcFullImage.Cursor = Cursors.SizeWE;
+					else if (flags == BoxFlags.Top || flags == BoxFlags.Bottom)
+						pcFullImage.Cursor = Cursors.SizeNS;
+					else if (flags == (BoxFlags.Top | BoxFlags.Left) || flags == (BoxFlags.Bottom | BoxFlags.Right))
+						pcFullImage.Cursor = Cursors.SizeNWSE;
+					else if (flags == (BoxFlags.Top | BoxFlags.Right) || flags == (BoxFlags.Bottom | BoxFlags.Left))
+						pcFullImage.Cursor = Cursors.SizeNESW;
+					else
+						pcFullImage.Cursor = Cursors.No;
+				}
+				else
+				{
+					pcFullImage.Cursor = System.Windows.Forms.Cursors.Default;
+				}
+			}
+		}
+
+		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+			if (_state == BoxState.Placing)
+			{
+				float ratiow = 176f / 264f;
+
+				_selection.Width = e.X - _selection.X;
+				_selection.Height = (int)(_selection.Width * ratiow);
+			}
+
+			if (_state != BoxState.None)
+			{
+				Bitmap b = new Bitmap(pbSource.Width, pbSource.Height);
+				Graphics g = Graphics.FromImage(b);
+				g.DrawImage(pcFullImage.Image, new Rectangle(0, 0, pbSource.Width, pbSource.Height), _selection.X, _selection.Y, _selection.Width, _selection.Height, GraphicsUnit.Pixel);
+				g.Dispose();
+
+				pbSource.Image = b;
+			}
+
+			if (_state != BoxState.None)
+			{
+				_state = BoxState.None;
+			}
+
+			pcFullImage.Refresh();
+		}
+
+		private void pcFullImage_Paint(object sender, PaintEventArgs e)
+		{
+			// Draw a rectangle displaying the current selection
+			Pen pen = Pens.GreenYellow;
+			e.Graphics.DrawRectangle(pen, _selection);
+
+			if (_state == BoxState.Moving)
+			{
+				e.Graphics.DrawRectangle(Pens.LimeGreen, _movement_point.X - 2, _movement_point.Y - 2, 4, 4);
+			}
+		}
+	}
+
+	public enum BoxState
+	{
+		None,
+		Placing,
+		Moving,
+		Resizing
+	}
+
+	[Flags]
+	public enum BoxFlags
+	{
+		None = 0,
+		Top = 0x1,
+		Bottom = 0x2,
+		Left = 0x4,
+		Right = 0x8,
+		All = 0xF
+	}
+}
